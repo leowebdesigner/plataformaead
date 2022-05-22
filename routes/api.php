87d172ea83724b46c4e042ef880b46689955298b
leowebdesigner <contato@leowebdesigner.com.br>
2022-05-22 02:04:52 -0300

    Create Modules

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\{
-   CourseController
+   CourseController,
+   ModuleController
 };
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/courses',[CourseController::class, 'index']);
 Route::get('/courses/{id}',[CourseController::class, 'show']);
+
+Route::get('/courses/{id}/modules/',[ModuleController::class, 'index']);
 
 Route::get('/', function(){
      return response()->json([
