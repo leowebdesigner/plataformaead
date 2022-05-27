693abe2c91cd919dae578c0e8888543dd9a822f1
leowebdesigner <contato@leowebdesigner.com.br>
2022-05-27 18:16:19 -0300

    Start Tests in Auth

---
+++ tests/Feature/Api/Auth/AuthTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature\Api\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Api\UtilsTrait;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    
+    use UtilsTrait;
+
+    public function test_fail_auth()
+    {
+        $response = $this->postJson('/auth',[]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_auth()
+    {
+        
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/auth',[
+            'email' => $user ->email,
+            'password' => 'password',
+            'device_name' => 'teste',
+        ]);
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_error_logout()
+    {
+
+        $response = $this->postJson('/logout');
+        
+        $response->assertStatus(401);
+    }
+
+    public function test_logout()
+    {
+       $token = $this -> createTokenUser();
+
+        $response = $this->postJson('/logout',[],[
+            'Authorization' => "Bearer {$token}",
+        ]);
+        
+        $response->assertStatus(200);
+    }
+
+    
+    public function test_error_get_me()
+    {
+
+        $response = $this->getJson('/me');
+        
+        $response->assertStatus(401);
+    }
+
+    public function test_get_me()
+    {
+
+        $token = $this -> createTokenUser();
+
+        $response = $this->getJson('/me', [
+            'Authorization' => "Bearer {$token}",
+        ]);
+        
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/Api/UtilsTrait.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace Tests\Feature\Api;
+
+use App\Models\User;
+
+trait UtilsTrait
+{
+
+    public function createTokenUser()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('teste')->plainTextToken;
+
+        return $token;
+    }
+
+}
