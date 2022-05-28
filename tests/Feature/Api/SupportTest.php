2fdec7ec0be7fc3294e3080f54b36903f5292fdf
leowebdesigner <contato@leowebdesigner.com.br>
2022-05-28 00:16:59 -0300

    Finish Tests with ReplySupport

--- tests/Feature/Api/SupportTest.php
+++ tests/Feature/Api/SupportTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\Lesson;
 use App\Models\Support;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -38,5 +39,74 @@ class SupportTest extends TestCase
 
         $response->assertStatus(200)
                  ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_unathenticated()
+    {
+        
+        $response = $this->getJson('/supports');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_get_supports()
+    {
+
+        Support::factory()->count(50)->create();
+        
+        $response = $this->getJson('/supports',$this->defaultHeaders());
+
+        $response->assertStatus(200)
+                 ->assertJsonCount(50, 'data');
+    }
+
+    public function test_get_supports_filter_lesson()
+    {
+        
+        $lesson = Lesson::factory()->create();
+
+        Support::factory()->count(50)->create();
+        Support::factory()->count(10)->create([
+            'lesson_id' => $lesson->id,
+        ]);
+
+        $payload =[
+            'lesson' => $lesson->id,
+        ];
+        
+        $response = $this->json('GET','/supports', $payload ,$this->defaultHeaders());
+
+        $response->assertStatus(200)
+                 ->assertJsonCount(10, 'data');
+    }
+
+    public function test_create_support_unathenticated()
+    {
+        $response = $this->postJson('/supports');
+
+        $response->assertStatus(401);
+    }
+
+    public function test_create_support_error_validations()
+    {
+        $response = $this->postJson('/supports',[], $this->defaultHeaders());
+
+        $response->assertStatus(422);
+    }
+
+    public function test_create_support()
+    {
+
+        $lesson = Lesson::factory()->create();
+
+        $payload = [
+            'lesson' => $lesson->id,
+            'status' => 'A',
+            'description' => 'dsaudhsau'
+        ];
+
+        $response = $this->postJson('/supports', $payload, $this->defaultHeaders());
+
+        $response->assertStatus(201);
     }
 }
